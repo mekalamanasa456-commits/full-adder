b7b2da83ec411f88597074b8e50be3abb54cb96e
mekalamanasa456-commits <mekalamanasa456@gmail.com>
2026-08-05 22:20:08 +0530

    Create full_adder.v

---
+++ full_adder.v
@@ -0,0 +1,14 @@
+full_adder.v
+module full_adder(
+    input A,
+    input B,
+    input Cin,
+    output Sum,
+    output Cout
+);
+
+assign Sum = A ^ B ^ Cin;
+
+assign Cout = (A & B) | (B & Cin) | (A & Cin);
+
+endmodule
